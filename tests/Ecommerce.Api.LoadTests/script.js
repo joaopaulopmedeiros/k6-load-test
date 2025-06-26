aaa2854706b6d082dd1e04ef03f50f4b7731f967
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2025-06-25 23:37:14 -0300

    chore: setup containers

--- tests/Ecommerce.Api.LoadTests/script.js
+++ tests/Ecommerce.Api.LoadTests/script.js
@@ -1,16 +1,16 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const target_vus = 10;
+const target_vus = 100;
 
 export const options = {
   thresholds: {
     http_req_duration: ['avg<500', 'med<500', 'min<100', 'max<2000'],
   },
   stages: [
-      { duration: "5s", target: target_vus },
-      { duration: "50s", target: target_vus },
-      { duration: "5s", target: 0 }
+    { duration: "5s", target: target_vus },
+    { duration: "50s", target: target_vus },
+    { duration: "5s", target: 0 }
   ]
 };
 
